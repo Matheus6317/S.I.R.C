919035211a6b24fc8373c407d9178f0258ca4d5e
Shirpss <mat631782@gmail.com>
2026-04-20 09:11:05 -0300

    Gerenciamento de API's

---
+++ script/PgClasses.js
@@ -0,0 +1,23 @@
+var http = require("http")
+
+var callback =  async function(request, response){
+    try{
+        let buscar = await fetch("https://www.dnd5eapi.co/api/2014/spells")
+        let arq = await buscar.json()
+    
+        response.writeHead(200, {"Content-Type": "application/json; charset=utf-8"})
+    
+        let strArq = JSON.stringify(arq)
+    
+        response.end(strArq)
+
+    }catch(error){
+        response.writeHead(500, {"Content-Type":"text/plain"})
+        response.end("Erro ao buscar Magias")
+    }
+
+}
+
+let server = http.createServer(callback)
+server.listen(3030)
+console.log("Servidor iniciado em http://localhost:3030/")
